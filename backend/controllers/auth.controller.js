f04618ccfa8c9e9af50260e9391b6a53832d8d7d
Md Yusuf <mdyusuf742171@gmail.com>
2024-06-09 11:50:05 +0530

    connect to mongodb

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req,res) => {
+    res.json({
+        data: "hey you hit signup API endpoint"
+    })
+}
+export const login = async (req,res) => {
+    res.json({
+        data: "hey you hit login API endpoint"
+    })
+}
+export const logout = async (req,res) => {
+    res.json({
+        data: "hey you hit logout API endpoint"
+    })
+}
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from 'express'
+import authRoutes from './routes/auth.routes.js'
+import connectMongoDB from './DB/connectMongoDB.js'
+import dotenv from "dotenv"
+
+
+dotenv.config()
 
 const app=express()
+const PORT = process.env.PORT || 500;
 
-app.get("/",(req,res) => {
-    res.send("Server is ready")
-})
+app.use("/api/auth",authRoutes)
 
-app.listen(8000,() => {
-    console.log('server is running on port 8000')
+
+app.listen(PORT,() => {
+    console.log(`server is running on port ${PORT}`)
+    connectMongoDB()
 })
