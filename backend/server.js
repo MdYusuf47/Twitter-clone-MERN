c5974db4f87597bde7f96390545708a8ac464f41
Md Yusuf <mdyusuf742171@gmail.com>
2024-06-15 11:08:30 +0530

    create follow and unfollow api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js'
 import connectMongoDB from './DB/connectMongoDB.js'
 import dotenv from "dotenv"
 import cookieParser from 'cookie-parser'
@@ -16,6 +17,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
+app.use("/api/user",userRoutes)
 
 
 app.listen(PORT,() => {
