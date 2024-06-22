3cfea18ce17190d7e680505cc70069e965931341
Md Yusuf <mdyusuf742171@gmail.com>
2024-06-22 11:33:26 +0530

    post routes and controllers api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express'
+
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
+import postRoutes from './routes/post.routes.js'
+
 import connectMongoDB from './DB/connectMongoDB.js'
 import dotenv from "dotenv"
 import cookieParser from 'cookie-parser'
@@ -27,6 +30,7 @@ app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
+app.use("/api/posts",postRoutes)
 
 
 app.listen(PORT,() => {
